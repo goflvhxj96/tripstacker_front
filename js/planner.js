c9ed2057ae69c1d3e86cf6b9bfdb207b568e39be
seongeun42 <gkapf838@gmail.com>
2023-09-10 23:29:52 +0900

    feat: 공공데이터 불러오기, 지도 이동, 지도에 마커 그리기 구현

--- js/planner.js
+++ js/planner.js
@@ -12,54 +12,204 @@
 
 
 
-// 생성자
+
+
+/**
+ * 지도 관련 함수
+ */
+
+// 초기 지도 : 중심 서울
+let map = drawMap(37.566535, 126.9779692, 9);
+let curX = 37.566535;
+let curY = 126.9779692;
+
+// 마커 배열
+let markers = [];
+let oneMarker;
+
+// 지도 띄우기
+function drawMap(x, y, lev) {
+    //지도를 담을 영역의 DOM 레퍼런스
+    let container = document.querySelector('.map');
+    let options = {
+        center: new kakao.maps.LatLng(x, y),
+        level: lev
+    };
+    
+    //지도 생성 및 객체 리턴
+    return new kakao.maps.Map(container, options);
+}
+
+// 지도 중심 좌표 이동 및 마커 생성
+function moveMapCenter(cnt) {
+    
+    // 이동할 위도 경도 위치 생성
+    let x = locations[cnt].mapx;
+    let y = locations[cnt].mapy;
+    
+    if (x == curX && y == curY) {
+        return;
+    }
+    
+    var latLng = new kakao.maps.LatLng(y, x);
+    
+    // 위치 이동
+    map.panTo(latLng);
+    curX = x;
+    curY = y;
+    
+    // 레벨 변경
+    map.setLevel(1);
+    
+    // 마커 생성
+    
+    
+    let marker = new kakao.maps.Marker({
+        position: latLng
+    });
+    marker.setMap(map);
+
+    // 기존 마커 삭제
+    if (oneMarker)
+        oneMarker.setMap(null);
+    oneMarker = marker;
+}
+
+// 지도 이동 함수
+function moveMap(x, y, lev) {
+    var latLng = new kakao.maps.LatLng(y, x);
+
+    // 위치 이동
+    map.panTo(latLng);
+    curX = x;
+    curY = y;
+
+    map.setLevel(lev);
+}
+
+// 마커 생성 함수
+function makeMarker(x, y, type) {
+    let latLng = new kakao.maps.LatLng(y, x);
+    let img = `../assets/marker/marker${type}.png`;
+    let size = new kakao.maps.Size(30, 30);
+    let markerImage = new kakao.maps.MarkerImage(img, size);
+
+    let marker = new kakao.maps.Marker({
+        position: latLng,
+        image: markerImage
+    });
+
+    return marker;
+}
+
+
+
+
+
+/**
+ * 공공데이터 관련 함수
+ */
+// 데이터 객체 생성자
 function LocationInfo(addr1, areacode, cotentid, cotenttypeid, firstimage, mapx, mapy, sigungucode, title, tel) {
     this.addr = addr1;
     this.areacode = areacode;
     this.cotentid = cotentid;
-    this.cotenttypeid = cotenttypeid;
+    this.cotenttypeid = parseInt(cotenttypeid);
     this.img = firstimage;
-    this.mapx = mapx;
-    this.mapy = mapy;
+    this.mapx = parseFloat(mapx);
+    this.mapy = parseFloat(mapy);
     this.sigungucode = sigungucode;
     this.title = title;
     this.tel = tel;
 }
 
 // 공공데이터 검색 결과 배열
-// locations = [];
-// test용
-locations = [];
-locations.push(new LocationInfo("서울 송파구 올림픽로 300¸ 2층", 1, 2924134, 38, "http://tong.visitkorea.or.kr/cms/resource/00/2879100_image2_1.jpg", 127.1040305171, 37.5142459111, 18, "가가밀라노 롯데백화점 에비뉴엘 월드타워점", ""));
-locations.push(new LocationInfo("충청남도 공주시 감영길 3", 34, 2750144, 38, "../assets/noImg.png", 127.1216721795, 36.4529297875, 1, "가가상점", ""));
-locations.push(new LocationInfo("경기도 안산시 상록구 감골로 173", 31, 2901530, 39, "http://tong.visitkorea.or.kr/cms/resource/12/2901512_image2_1.jpg", 126.8580034344, 37.2971166997, 15, "가가목장", ""));
-
-
+let locations = [];
 
 // 공공데이터 가져오기
-{
-    const base = "https://apis.data.go.kr/B551011/KorService1/";
-    const key = "NYi8pVbtWFuARtVe2wCP7BkiQ5Rhmc0wd3AKE5UanrA5d%2F3m%2BCWTbF5Ur9NFxR%2BBL5hAZFbnREL2bd8X5pj6sA%3D%3D"; 
-}
+const base = "https://apis.data.go.kr/B551011/KorService1/areaBasedList1";
+const serviceKey = "NYi8pVbtWFuARtVe2wCP7BkiQ5Rhmc0wd3AKE5UanrA5d%2F3m%2BCWTbF5Ur9NFxR%2BBL5hAZFbnREL2bd8X5pj6sA%3D%3D"; 
 
+async function getSearchData() {
+    // 마커 지워주기
+    markers.forEach(m => {
+        m.setMap(null);
+    });
 
-// 지도 띄우기
-{
-    //지도를 담을 영역의 DOM 레퍼런스
-    let container = document.querySelector('.map');
-    let options = { //지도를 생성할 때 필요한 기본 옵션
-        // 중심 : 서울
-        //지도의 중심좌표
-        center: new kakao.maps.LatLng(37.566535, 126.9779692),
-        //지도의 레벨(확대, 축소 정도)
-        level: 9
-    };
+    let numOfRows = 20;
+    let pageNo = 1;
+    const keyword = document.querySelector("#search").value;
+    const sido = document.querySelector("#sidoSelect");
+    const areaCode = sido.options[sido.selectedIndex].value;
+    const gungu = document.querySelector("#gugunSelect")
+    const sigunguCode = gungu.options[gungu.selectedIndex].value;
+    const types = document.querySelectorAll("input[name='type']:checked");
+
+    if (keyword.length == 0 && areaCode.length == 0 && sigunguCode == 0 && types.length == 0) {
+        alert("검색 조건을 입력해주세요.");
+        return;
+    }
+
+    let res = [];
+    if (types.length > 0) {
+        for (i = 0; i < types.length; i++) {
+            const typeId = parseInt(types[i].value);
+            const url = `${base}?serviceKey=${serviceKey}&numOfRows=${numOfRows}&pageNo=${pageNo}&MobileOS=ETC&MobileApp=AppTest&_type=json&contentTypeId=${typeId}&areaCode=${areaCode}&sigunguCode=${sigunguCode}`;
+            res = res.concat(await getPublicData(url));
+        }
+    } else {
+        const url = `${base}?serviceKey=${serviceKey}&numOfRows=${numOfRows}&pageNo=${pageNo}&MobileOS=ETC&MobileApp=AppTest&_type=json&contentTypeId=&areaCode=${areaCode}&sigunguCode=${sigunguCode}`;
+        res = await getPublicData(url);
+    }
     
-    //지도 생성 및 객체 리턴
-    let map = new kakao.maps.Map(container, options);
+    locations = res;
+    if (keyword.length > 0)
+        locations = locations.filter(o => o.title.includes(keyword));
+
+    // html에 리스트 그리기
+    makeLocationList();
+
+    // 지역 검색할 때는 지도에 그리기
+    if (areaCode.length != 0) {
+        // 마커 생성
+        let tmp = [];
+        locations.forEach(o => {
+            let m = makeMarker(o.mapx, o.mapy, o.cotenttypeid);
+            tmp.push(m);
+            m.setMap(map);
+        });
+        markers = tmp;
+
+        // 지도 이동
+        moveMap(locations[0].mapx, locations[0].mapy, 10);
+    }
+}
+
+// 공공데이터 배열로 만들기
+async function getPublicData(url) {
+    let arr = [];
+    await fetch(url)
+    .then(res => res.json())
+    .then(r => {
+        r.response.body.items.item.forEach(o => {
+            addr = o.addr1;
+            if (o.addr2 != "") {
+                addr += " " + o.addr2;
+            }
+            img = o.firstimage != "" ? o.firstimage : "../assets/noImg.png";
+            arr.push(new LocationInfo(addr, o.areacode, o.contentid, o.contenttypeid, img, o.mapx, o.mapy, o.sigungucode, o.title, o.tel));
+        });
+    });
+    return arr;
 }
 
 
+
+
+
+/**
+ * planner.html 조작 관련 함수
+ */
 
 // 일수 별 일정 클릭시 현재 일정을 해당 일정으로 변경하는 함수
 curDay = "day1";
@@ -72,25 +222,18 @@ function addEventToDayPlan(day) {
 }
 
 
-
-
-/*
-    html에 리스트 만드는 함수
-*/
-
-makeLocationList(locations);
-
 // 공공데이터 검색 결과 리스트 만들기
-function makeLocationList(data) {
+makeLocationList();
+function makeLocationList() {
     locationList = document.querySelector(".location-list ul");
     content = `
-        <div id="listCnt">전체 ${data.length}개</div>
+        <div id="listCnt">전체 ${locations.length}개</div>
     `;
-
+    
     cnt = 0;
-    data.forEach((o) => {
+    locations.forEach((o) => {
         content += 
-        `<li id="llt${cnt}">
+        `<li id="llt${cnt}" onclick="moveMapCenter(${cnt})">
             <div>
             <img
                 src="${o.img}"
